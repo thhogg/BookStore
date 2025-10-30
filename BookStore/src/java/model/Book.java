73d1f71e53f1d0baeb33dc2636f7ba1c36030889
thhoang <hoang230116@gmail.com>
2025-10-30 21:57:43 +0700

    add some models

--- BookStore/src/java/model/Book.java
+++ BookStore/src/java/model/Book.java
@@ -4,10 +4,130 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author Leo
  */
 public class Book {
+    private int bookID;
+    private String title;
+    private int authorID;
+    private int publisherID;
+    private int categoryID;
+    private String isbn;
+    private int price;
+    private int stock;
+    private String description;
+    private String imageUrl;
+    private Date createdAt;
+
+    public Book() {
+    }
+
+    public Book(int bookID, String title, int authorID, int publisherID, int categoryID, String isbn, int price, int stock, String description, String imageUrl, Date createdAt) {
+        this.bookID = bookID;
+        this.title = title;
+        this.authorID = authorID;
+        this.publisherID = publisherID;
+        this.categoryID = categoryID;
+        this.isbn = isbn;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public int getPublisherID() {
+        return publisherID;
+    }
+
+    public void setPublisherID(int publisherID) {
+        this.publisherID = publisherID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
     
 }
